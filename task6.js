e4064bfa2e17914da40bf8a4e30ba1cb0dae39c3
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-03-28 15:18:58 +0500

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,9 @@
+const removeZero=function(nums){
+    let end = nums.length-1
+    for(let i =end;i>=0;i--){
+        if(nums[i]===0){
+            nums.splice(i,1)
+            nums.push(0)
+        }
+    }
+}
