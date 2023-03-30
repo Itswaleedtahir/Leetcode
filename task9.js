ff8cffb5a751b50f50b5dd448f736112a9ac899c
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-03-30 09:51:38 +0500

    Create task9.js

---
+++ task9.js
@@ -0,0 +1,11 @@
+const equal = function(s,t){
+    if(s.length>t.length)return  false;
+    const t_length=t.length;
+    let sub=0
+    for(let i=0 ; i<t_length ; i++){
+        if(s[sub]===t[i]){
+            sub++
+        }
+    }
+    return sub === s.length
+}
