0f6454752ab427355b2147b52a25b162113cb71f
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-03-30 10:18:57 +0500

    Update task11.js

--- task11.js
+++ task11.js
@@ -2,6 +2,9 @@ const reverse = function(head){
     let priv,cur=head,next
     while(cur){
         next = cur.next
-        
+        cur.next = priv;
+        priv = next
+        cur = next
     }
+    return priv
 }
