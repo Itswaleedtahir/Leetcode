dc468cd110e3ffee665fb7466a276ccfc4820a19
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-03-30 10:01:39 +0500

    Create task10.js

---
+++ task10.js
@@ -0,0 +1,9 @@
+const merge= function(l1,l2){
+    if(!l1 || !l2) return li || l2;
+    if(l1.val<l2.val){
+        l1.next = mergeTwoLists(l1.next,l2)
+        return l1
+    }
+    l2.next = mergeTwoLists(l1,l2.next)
+    return  l2;
+}
